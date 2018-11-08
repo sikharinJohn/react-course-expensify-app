04b6e484ce54e506f6a131fd15d204436e931c31
Sikharin John <sikharin.wandee@gmail.com>
2018-11-08 14:03:21 +0700

    Add privacy page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,25 +6,28 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
+import PrivacyPage from '../components/PrivacyPage';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
+
 export const history = createHistory();
 
-const AppRouter = () =>   (
+const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <PublicRoute path="/" component={LoginPage} exact={true}  />
-                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}  />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
                 <PrivateRoute path="/create" component={AddExpensePage} />
                 <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+                <PublicRoute path="/privacy" component={PrivacyPage} />
                 <PublicRoute component={NotFoundPage} />
             </Switch>
 
         </div>
 
-       
+
     </Router>
 );
 
